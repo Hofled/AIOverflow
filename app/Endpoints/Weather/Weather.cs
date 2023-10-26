55c475e85a9cd9dea2f411560556589124357623
Hofled <ofri.herold@gmail.com>
2023-10-26 18:33:28 +0300

    updated the weather endpoint to be based on `ControllerBase`

--- app/Endpoints/Weather/Weather.cs
+++ app/Endpoints/Weather/Weather.cs
@@ -1,7 +1,21 @@
-public static class WeatherEndpoint {
-    public static void Map(WebApplication app) {
-        var _group = app.MapGroup("/weather");
-        _group.MapGet("/", () => "Weather Endpoint");
-        _group.MapGet("/{city}", (string city) => $"The weather for {city} is sunny");
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("weather")]
+[Authorize]
+public class WeatherController : ControllerBase
+{
+    private readonly ILogger<WeatherController> _logger;
+
+    public WeatherController(ILogger<WeatherController> logger)
+    {
+        _logger = logger;
     }
+
+    [HttpGet]
+    public string Weather() => "Weather Endpoint";
+
+    [HttpGet("{city}")]
+    public string City(string city) => $"The weather for {city} is sunny";
 }
