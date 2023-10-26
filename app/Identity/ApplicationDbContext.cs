795b6c693014cf921fbd24b8391270a1db20868a
Hofled <ofri.herold@gmail.com>
2023-10-26 12:35:20 +0300

    added `ApplicationDbContext` & `ApplicationUser` initial models

---
+++ app/Identity/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    {
+    }
+}
