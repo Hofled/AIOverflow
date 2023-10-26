d0da6412399d9817d6328b2a96ba92d73b07c002
Hofled <ofri.herold@gmail.com>
2023-10-26 18:32:59 +0300

    updated the application setup & DI services

--- app/Program.cs
+++ app/Program.cs
@@ -1,29 +1,58 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+ConfigureServices(builder);
+
 var app = builder.Build();
 
-app.UseHttpLogging();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-RegisterEndpoints(app);
+ConfigureApp(app);
 
 app.MapFallbackToFile("index.html");
 
 app.Run();
 
-static void RegisterEndpoints(WebApplication app)
+void ConfigureServices(WebApplicationBuilder builder)
 {
-    WeatherEndpoint.Map(app);
+    builder.Services.AddControllers();
+
+    string? DbConnectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+    if (DbConnectionString == null)
+    {
+        throw new Exception("ConnectionString is null");
+    }
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(DbConnectionString));
+
+    builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+    builder.Services.AddIdentityServer().AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddIdentityServerJwt();
+}
+
+void ConfigureApp(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpLogging();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseAuthentication();
+    app.UseIdentityServer();
+    app.UseAuthorization();
+    app.MapControllers();
 }
