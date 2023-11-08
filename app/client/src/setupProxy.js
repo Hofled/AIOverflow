7d1a2c5c7a55769e18c38541b8c5d1da7341cbe1
Hofled <ofri.herold@gmail.com>
2023-11-09 00:23:19 +0200

    added `/_configuration` as a proxied path

--- app/client/src/setupProxy.js
+++ app/client/src/setupProxy.js
@@ -6,6 +6,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const context = [
   "/weather",
+  "/_configuration",
 ];
 
 const onError = (err, req, resp, target) => {
