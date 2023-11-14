a53b7b987e60aa3ed86e4395e11efb61ab54dce2
Hofled <ofri.herold@gmail.com>
2023-11-14 23:30:17 +0200

    updated proxy to include the `/Identity` endpoint

--- app/client/src/setupProxy.js
+++ app/client/src/setupProxy.js
@@ -7,6 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const context = [
   "/weather",
   "/_configuration",
+  "/Identity",
 ];
 
 const onError = (err, req, resp, target) => {
