43078b962c8da83508445e475a26f496063adaf8
Hofled <ofri.herold@gmail.com>
2023-12-31 18:03:11 +0100

    added initial seeded administrator user

---
+++ database/seed.sql
@@ -0,0 +1,3 @@
+-- The hashed password is 'verysecurepassword'
+INSERT INTO "Users" ("Id", "Name", "PasswordHash")
+VALUES (1, 'administrator', 'AQAAAAIAAYagAAAAEHdDTPR69sevaAKq6hrQ831BXctHcaqjVZFHZIFA3nZt7RjCzTmAzAZiRhZmZwYxog==');
