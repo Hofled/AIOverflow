049f07093ca7f2b5ec262fa5371e136052ec992c
Hofled <ofri.herold@gmail.com>
2023-12-31 18:25:55 +0100

    updated administrator account related seed data

--- database/seed.sql
+++ database/seed.sql
@@ -1,3 +1,5 @@
 -- The hashed password is 'verysecurepassword'
 INSERT INTO "Users" ("Id", "Name", "PasswordHash")
-VALUES (1, 'administrator', 'AQAAAAIAAYagAAAAEHdDTPR69sevaAKq6hrQ831BXctHcaqjVZFHZIFA3nZt7RjCzTmAzAZiRhZmZwYxog==');
+VALUES (-1, 'administrator', 'AQAAAAIAAYagAAAAEHdDTPR69sevaAKq6hrQ831BXctHcaqjVZFHZIFA3nZt7RjCzTmAzAZiRhZmZwYxog==');
+INSERT INTO "UserClaim" ("Id", "Type", "Value", "UserId")
+VALUES (-1, 'role', 'admin', -1);
