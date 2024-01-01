fe4b3e77e6ccbe093fb3ac2a0791fa4b65cdff83
Hofled <ofri.herold@gmail.com>
2024-01-01 03:53:14 +0100

    added static files and fallback middlewares

--- app/Program.cs
+++ app/Program.cs
@@ -8,6 +8,7 @@ AIOverflow.Identity.Services.ConfigureServices(builder);
 AddDbContext(builder);
 
 var app = builder.Build();
+app.MapFallbackToFile("index.html");
 
 AIOverflow.Identity.Endpoints.ConfigureIdentity(app);
 UseAppMiddlewares(app);
@@ -29,6 +30,15 @@ static void AddDbContext(WebApplicationBuilder builder)
 
 static void UseAppMiddlewares(WebApplication app)
 {
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
     app.UseHttpLogging();
     app.UseAuthentication();
     app.UseAuthorization();
