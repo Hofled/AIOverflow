93f5e7d489c06d8bbdcc6bda1808a7f12285ef7d
Hofled <ofri.herold@gmail.com>
2024-02-22 00:22:54 +0100

    added more user claims to the seed

--- database/seed.sql
+++ database/seed.sql
@@ -3,3 +3,5 @@ INSERT INTO "Users" ("Id", "Name", "PasswordHash")
 VALUES (-1, 'administrator', 'AQAAAAIAAYagAAAAEHdDTPR69sevaAKq6hrQ831BXctHcaqjVZFHZIFA3nZt7RjCzTmAzAZiRhZmZwYxog==');
 INSERT INTO "UserClaim" ("Id", "Type", "Value", "UserId")
 VALUES (-1, 'role', 'admin', -1);
+INSERT INTO "UserClaim" ("Id", "Type", "Value", "UserId")
+VALUES (-2, 'username', 'administrator', -1);
