f1efa1c970c750fc51615e1f36f245bd55e6d227
Hofled <ofri.herold@gmail.com>
2024-03-31 22:27:51 +0200

    added posts service

---
+++ app/client/src/services/posts/service.ts
@@ -0,0 +1,13 @@
+import { AxiosResponse } from "axios";
+import { OperationStatus, axiosRequest, wrapFail, wrapSuccess } from "../axios";
+import { Post } from "./models";
+
+class PostsService {
+    async getPosts(): Promise<OperationStatus<Post[]>> {
+        return axiosRequest<Post[], any>("posts", "GET", (r: AxiosResponse<Post[]>) => wrapSuccess(r.data), (r) => wrapFail(r));
+    }
+}
+
+const postsService = new PostsService()
+
+export default postsService;
